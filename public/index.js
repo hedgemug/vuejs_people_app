df2c04dbbf3eb785c8640720a4e32e76c0d27b01
Dani Zaghian <dani.zaghian@gmail.com>
2018-02-18 09:44:10 -0800

    initial commit

---
+++ public/index.js
@@ -0,0 +1,58 @@
+/* global Vue, VueRouter, axios */
+
+var HomePage = {
+  template: "#home-page",
+  data: function() {
+    return {
+      people: [
+        {
+          name: "Bob",
+          bio: "Small batch salvia Marfa chillwave delectus, odio forage art party laborum street art minim fixie locavore hoodie mollit.",
+          bioVisible: true
+        },
+        {
+          name: "Alice",
+          bio: "Tattooed letterpress gluten-free ugh, adipisicing scenester church-key gentrify tousled gastropub pour-over Shoreditch asymmetrical lomo High Life.",
+          bioVisible: true
+        },
+        {
+          name: "Tibor",
+          bio: "Incididunt photo booth ethical reprehenderit adipisicing. Echo Park readymade Bushwick distillery Tonx. +1 semiotics qui duis literally.",
+          bioVisible: true
+        },
+        {
+          name: "Živa",
+          bio: "Excepteur shabby chic semiotics Marfa, quinoa try-hard polaroid pariatur banh mi selfies incididunt brunch trust fund. Ethical dolor PBR&B Tumblr.",
+          bioVisible: true
+        }
+      ],
+      newPerson: {name: "", bio: "", bioVisible: true}
+    };
+  },
+  created: function() {},
+  methods: {
+    addPerson: function() {
+      this.people.push(this.newPerson);
+    },
+    deletePerson: function(person) {
+      var index = this.people.indexOf(person);
+      this.people.splice(index, 1);
+    },
+    toggleBio: function(person) {
+      person.bioVisible = !person.bioVisible;
+    }
+  },
+  computed: {}
+};
+
+var router = new VueRouter({
+  routes: [{ path: "/", component: HomePage }],
+  scrollBehavior: function(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  }
+});
+
+var app = new Vue({
+  el: "#vue-app",
+  router: router
+});
